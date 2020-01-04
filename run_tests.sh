be03d07aa1924cfea497a9b895f58061ac97a511
capital-G <dennis.scheiba@gmx.de>
2020-01-04 17:27:27 +0100

    add test suite and coverage reporting tool

--- run_tests.sh
+++ run_tests.sh
@@ -3,10 +3,17 @@
 pip install --quiet -r requirements.txt -r requirements-dev.txt
 
 echo 'Run pep8 checks'
-flake8 .
+flake8 start.py loco_sound
 
 echo '\nRun mypy checks'
 mypy start.py loco_sound
+
+echo '\nRun tests'
+coverage run
+
+echo '\nGenerate coverage report'
+coverage html
+coverage report
 
 echo '\nBuild HTML docs'
 make -C docs/ --quiet html
